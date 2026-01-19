0879f8368d052966ca2ae7be3313f6b325b221fe
alimaashi <aashimbayeva1@gmail.com>
2026-01-19 20:13:12 +0500

    Added custom grid sizing

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-const numRows = 16;
-const numCols = 16;
 const gridContainer = document.querySelector(".grid-container");
+const selectSizeButton = document.querySelector(".select-size-button");
+const maxGridNum = 100;
+const minGridNum = 1;
 
-for (let i = 0; i < numRows; i++) {
-    const newRow = document.createElement("div");
-    newRow.classList.add("grid-row-container");
-    newRow.style.height = `${100/numRows}%`;
-
-    for (let j = 0; j < numCols; j++) {
-        const newGridCell = document.createElement("div");
-        newGridCell.classList.add("grid-cell");
-        newRow.appendChild(newGridCell);
+function createNewGrid(gridSize) {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
     }
-    gridContainer.appendChild(newRow);
+
+    for (let i = 0; i < gridSize; i++) {
+        const newRow = document.createElement("div");
+        newRow.classList.add("grid-row-container");
+        newRow.style.height = `${100/gridSize}%`;
+
+        for (let j = 0; j < gridSize; j++) {
+            const newGridCell = document.createElement("div");
+            newGridCell.classList.add("grid-cell");
+            newGridCell.addEventListener("mouseenter", () => {
+                newGridCell.style.backgroundColor = "red";
+            })
+            newRow.appendChild(newGridCell);
+        }
+        gridContainer.appendChild(newRow);
+    }
 }
+
+function selectGridSize() {
+    let gridSize = prompt("Enter number of squares per side for the new grid.");
+    if (Number.isInteger(parseInt(gridSize)) && (gridSize <= maxGridNum) && (gridSize >= minGridNum)) {
+        return gridSize;
+    }
+    alert("The number of squares must be between 1 and 100!");
+    return -1;
+}
+
+createNewGrid(16); // By default load a 16x16 grid
+
+selectSizeButton.addEventListener("click", () => {
+    gridSize = selectGridSize();
+
+    if (gridSize != -1) {
+        createNewGrid(gridSize);
+    }
+})
